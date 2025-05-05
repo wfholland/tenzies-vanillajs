d995b7466215a20488daf125cdd2463c0c8e3f38
William Holland <210418052+wfholland@users.noreply.github.com>
2025-05-05 19:07:20 -0400

    Add dice value randomization and roll button functionality

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const diceContainer = document.querySelector(".dice-container");
 const rollButton = document.querySelector("#roll-button");
 
-// dynamically create 10 new divs
+// dynamically create 10 new dice
 
 function renderDice() {
+  diceContainer.innerHTML = "";
   for (let i = 0; i < 10; i++) {
+    const randomValue = Math.ceil(Math.random() * 10);
     const diceEl = document.createElement("div");
     diceEl.classList.add("dice-element");
-    diceEl.textContent = "1";
+    diceEl.dataset.value = randomValue;
+    diceEl.textContent = randomValue;
     diceContainer.appendChild(diceEl);
   }
 }
 
 renderDice();
+rollButton.addEventListener("click", renderDice);
