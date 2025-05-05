dce58d3848efacfa0ec59426c256428e260f6ed4
William Holland <210418052+wfholland@users.noreply.github.com>
2025-05-05 19:47:47 -0400

    Add dice click logic, functionality, and styles for held dice

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const diceContainer = document.querySelector(".dice-container");
 const rollButton = document.querySelector("#roll-button");
 
+const heldDice = [];
+
 // dynamically create 10 new dice
 
 function renderDice() {
@@ -10,10 +12,24 @@ function renderDice() {
     const diceEl = document.createElement("div");
     diceEl.classList.add("dice-element");
     diceEl.dataset.value = randomValue;
+    diceEl.dataset.isHeld = "false";
     diceEl.textContent = randomValue;
     diceContainer.appendChild(diceEl);
   }
 }
 
-renderDice();
+// Add click event listener to roll dice button
 rollButton.addEventListener("click", renderDice);
+
+// dice click handler logic
+function handleHeldDie(event) {
+  const die = event.target;
+  die.dataset.isHeld === "false"
+    ? (die.classList.add("held-dice"), (die.dataset.isHeld = "true"))
+    : (die.classList.remove("held-dice"), (die.dataset.isHeld = "false"));
+}
+renderDice();
+
+// Add dice click logic to all die
+const allDice = document.querySelectorAll(".dice-element");
+allDice.forEach((die) => die.addEventListener("click", handleHeldDie));
