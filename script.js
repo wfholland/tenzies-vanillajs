2ab69f6b5b5b47bfa7311430056c8bc00c6acf5f
William Holland <210418052+wfholland@users.noreply.github.com>
2025-05-06 11:25:43 -0400

    Refactor variable and function names for clarity

--- script.js
+++ script.js
@@ -2,27 +2,30 @@ const diceContainer = document.querySelector(".dice-container");
 const rollButton = document.querySelector("#roll-button");
 
 // Array to track state of held die
-let heldDice = [];
+let heldDiceList = [];
 
 // dynamically create 10 new dice
 
 function renderDice() {
   diceContainer.innerHTML = "";
+
   for (let i = 0; i < 10; i++) {
-    const isHeld = heldDice.some((die) => die.id === i.toString());
-    const randomValue = Math.ceil(Math.random() * 10);
-    const diceEl = document.createElement("div");
-    const currentHeldDie = heldDice.find((die) => die.id === i.toString());
-    const diceValue = isHeld ? currentHeldDie.value : randomValue;
-    diceEl.dataset.id = i.toString();
-    diceEl.classList.add("dice-element");
-    isHeld ? diceEl.classList.add("held-dice") : "";
-    diceEl.dataset.value = diceValue;
-    diceEl.textContent = diceValue;
-    diceContainer.appendChild(diceEl);
+    const isDieHeld = heldDiceList.some((die) => die.id === i.toString());
+    const randomDieValue = Math.ceil(Math.random() * 10);
+    const dieElement = document.createElement("div");
+    const currentHeldDie = heldDiceList.find((die) => die.id === i.toString());
+    const dieValue = isDieHeld ? currentHeldDie.value : randomDieValue;
+    dieElement.dataset.id = i.toString();
+    dieElement.classList.add("dice-element");
+    if (isDieHeld) dieElement.classList.add("held-dice");
+    dieElement.dataset.value = dieValue;
+    dieElement.textContent = dieValue;
+    diceContainer.appendChild(dieElement);
   }
-  const allDice = document.querySelectorAll(".dice-element");
-  allDice.forEach((die) => die.addEventListener("click", handleHeldDie));
+  const allDiceElements = document.querySelectorAll(".dice-element");
+  allDiceElements.forEach((die) =>
+    die.addEventListener("click", handleHeldDie)
+  );
 }
 
 // Add click event listener to roll dice button
@@ -34,13 +37,15 @@ function handleHeldDie(event) {
   const dieValue = die.dataset.value;
   const dieID = die.dataset.id;
   manageDiceArray(dieID, dieValue);
-  const isHeld = heldDice.some((die) => die.id === dieID);
-  die.classList.toggle("held-dice", isHeld);
+  const isDieHeld = heldDiceList.some((die) => die.id === dieID);
+  die.classList.toggle("held-dice", isDieHeld);
 }
 
 function manageDiceArray(id, value) {
-  const filteredDice = heldDice.filter((die) => die.id !== id);
-  const isHeld = heldDice.some((die) => die.id === id);
-  !isHeld ? heldDice.push({ id: id, value: value }) : (heldDice = filteredDice);
+  const filteredDice = heldDiceList.filter((die) => die.id !== id);
+  const isDieHeld = heldDiceList.some((die) => die.id === id);
+  !isDieHeld
+    ? heldDiceList.push({ id: id, value: value })
+    : (heldDiceList = filteredDice);
 }
 renderDice();
